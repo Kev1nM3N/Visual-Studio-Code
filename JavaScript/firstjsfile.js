76307c70d37eb38193000429de3dd2c225051c67
Kevin Mendez <mediaman502@aol.com>
2022-12-02 14:31:30 -0500

    learning for loops

--- JavaScript/firstjsfile.js
+++ JavaScript/firstjsfile.js
@@ -1,6 +1,3 @@
-let cash = 50;
-let price = 40;
-let isStoreOpen = true;
-
-let str = cash >= price && isStoreOpen ? `print receipt` : `Do not print`
-console.log(str);
+for (let i = 0; i < 100; i++){
+    console.log(i + 1);
+}
