e632cf717cf3809b9801352f8db46cfd9ece7e9a
Kevin Mendez <mediaman502@aol.com>
2022-12-17 12:49:39 -0500

    learning hard arrays

--- JavaScript/firstjsfile.js
+++ JavaScript/firstjsfile.js
@@ -1,15 +1,22 @@
-let animals = [`Cat`, `Dog`, `Parrot`, `Snake`, `Hamster`]
+let grades = [`A+`, `A`, `B`, `FAIL`]
+
+let goodGrades = []
+
+for (let i = 0; i < grades.length; i++){
+    if (grades[i] !== `FAIL`){
+        goodGrades.push(grades[i])
+    }
+}
+
+console.log(goodGrades)
+
 
 let dollars = [1, 5, 10, 3]
 
-let newAnimals = animals.filter(element => element !== `Snake`)
-
-console.log(newAnimals);
-
 cents = []
 
-for(let i = 0; i < dollars.length; i++){
+for (let i = 0; i < dollars.length; i++){
     cents.push(dollars[i] * 100)
 }
 
-console.log(cents)
+console.log(cents);
