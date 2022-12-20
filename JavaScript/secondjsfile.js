5da51e9118b0eb71f5a56fb66dc71b56c0c8626c
Kevin Mendez <mediaman502@aol.com>
2022-12-20 17:43:55 -0500

    went over the DOM!

--- JavaScript/secondjsfile.js
+++ JavaScript/secondjsfile.js
@@ -1,24 +1,3 @@
-/**
- * Write a for loop that loops through 1 -20
- * if the number is divisble by 3, print "Frontend"
- * if the number is divisble by 5, print "Simplified"
- * if the number is divisble by 3 and 5, print "Frontend Simplified"
- * If the Number is *not* divisible by either 3 or 5, print the number
- * 
- * Example:
- * 1 -> 1
- * 2 -> 2
- * 3 -> "Frontend"
- * 4 -> 4
- * 5 -> "Simplified"
- * ...
- * 15 -> "Frontend Simplified"
- * ...
- * 20 -> "Simplified"
- */
-
-const str = `Frontend Simplifed`;
-
-for (i = 0; i < str.length; i++){
-    console.log(str[i]);
+function toggleDarkMode(){
+    document.querySelector(`body`).classList.toggle(`dark-theme`)
 }
