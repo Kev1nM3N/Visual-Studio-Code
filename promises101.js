c4b105c7672c9dc73cabe18e5d8729353ef70a94
Kevin Mendez <mediaman502@aol.com>
2022-12-22 22:09:14 -0500

    for practice writing promises

--- promises101.js
+++ promises101.js
@@ -1,10 +1,20 @@
-// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"));
-const emailRef = document.querySelector(".email")
-console.log(emailRef)
+// const emailRef = document.querySelector(`.email`); //We select which element to use in HTML
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => { //inside the arguement is where we get the response from the backend server. We can name it whatever but response is best practice.
-    response.json().then(data => {
-        console.log(data)
-        emailRef.innerHTML = data.email
-    }) //to make this compatible with frontend we add json followed by parenthesis. This makes it readable.
-})
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {                  //We fetch the data, use the .then to ask for data, call it response
+//     return response.json();              //This entire thing is now response.json()
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+//   });
+
+// async function main(){
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")     //await automatically returns the response for us.
+//     const data = await response.json()       //Right now, its gibberish data. To make frontend understand this, we add .json()
+//     console.log(data);
+//     emailRef.innerHTML = data.email;
+// }
+
+// main()
+
