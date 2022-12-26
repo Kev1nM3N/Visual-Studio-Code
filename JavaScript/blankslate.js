304dd63dcc5d2cc7424d10f63598b5960f6fc155
Kevin Mendez <mediaman502@aol.com>
2022-12-25 20:30:52 -0500

    learning

---
+++ JavaScript/blankslate.js
@@ -0,0 +1,10 @@
+// function ageSecondsInput(age){
+//     let secondsInAHour = 60 * 60;
+//     let secondsInADay = secondsInAHour * 24
+//     let secondsInAYear = secondsInADay * 365
+//     let secondsLived = secondsInAYear * age
+//     return secondsLived
+// }
+
+// console.log(ageSecondsInput(26));
+
