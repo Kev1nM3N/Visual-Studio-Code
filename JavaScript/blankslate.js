3d466fca44a55c0dd9c09bf33458ca822bd3baf3
Kevin Mendez <mediaman502@aol.com>
2023-01-20 17:25:05 -0500

    on to advance javascript

---
+++ JavaScript/blankslate.js
@@ -0,0 +1,12 @@
+function showRating(rating){
+    let ratings = ``
+    for (let i = 0; i < rating; i++){
+        ratings = ratings + `*`
+        if (i !== rating - 1){
+            ratings = ratings + ` `
+        }
+    }
+    return ratings
+}
+
+console.log(showRating(4));
