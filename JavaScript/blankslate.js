5c3cbe1c21351020f473229e37e569d9cd528ea0
Kevin Mendez <mediaman502@aol.com>
2023-01-22 14:20:41 -0500

    played around and worked

--- JavaScript/blankslate.js
+++ JavaScript/blankslate.js
@@ -1,16 +1,13 @@
-function showRatings(rating){
-    let ratings = ``
-    for (let i = 0; i < Math.floor(rating); i++){
-        ratings += `*`
-        if (i !== Math.floor(rating) - 1){
-            ratings += ` `
-        }
-    }
-
-    if (!Number.isInteger(rating)){
-        ratings += ` .`
-    }
-    return ratings
+function sortHighToLow(numbers){
+    return numbers.sort((a, b) => b.price - a.price);
 }
 
-console.log(showRatings(4.5));
+console.log(
+    sortHighToLow([
+        {id: 1, price: 50},
+        {id: 2, price: 30},
+        {id: 3, price: 60},
+        {id: 4, price: 10},
+
+    ])
+);
