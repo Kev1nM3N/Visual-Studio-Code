fcb81e705b4ebf9c0e94b3921e67e6db8de7c3f0
Kevin Mendez <mediaman502@aol.com>
2023-01-29 14:50:03 -0500

    updated for more

---
+++ Week 4/adportfolio.js
@@ -0,0 +1,16 @@
+function video (seconds){
+    let timerMinutes = Math.floor(seconds / 60)
+    let timerSeconds = seconds % 60
+
+    if (timerMinutes.toString().length === 1){
+        timerMinutes = `0` + timerMinutes
+    }
+
+    if (timerSeconds.toString().length === 1){
+        timerSeconds = `0` + timerSeconds
+    }
+
+    return timerMinutes + `:` + timerSeconds
+}
+
+console.log(video(300))
