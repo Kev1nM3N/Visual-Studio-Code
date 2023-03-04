28dcf7267fe936beb83e227b454d6e1ab9979466
Kevin Mendez <mediaman502@aol.com>
2023-03-04 10:38:15 -0500

    did my duty

--- Javascript E-Commerce/books.js
+++ Javascript E-Commerce/books.js
@@ -1,11 +1,23 @@
-function renderBooks (){
+function renderBooks (filter){
   const booksWrapper = document.querySelector(`.books`);
 
 
   const books = getBooks();
 
+  if (filter === `LOW-TO-HIGH`){
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+  else if (filter === `HIGH-TO-LOW`){
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }
+  else if (filter === `RATING`){
+    books.sort((a, b) => b.rating - a.rating);
+  }
 
-  const booksHTML = books.map(book => {
+
+
+  const booksHTML = books
+  .map(book => {
     return `<div class="book">
       <figure class="book__img--wrapper">
           <img class="book__img" src="${book.url}" alt="">
@@ -14,22 +26,32 @@ function renderBooks (){
           ${book.title}
       </div>
       <div class="book__ratings">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star-half-alt"></i>
+          ${ratingsHTML(book.rating)}
       </div>
       <div class="book__price">
-          <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+          <span>$${book.originalPrice.toFixed(2)}</span> $
       </div>
     </div>`
   }).join(``);
 
   booksWrapper.innerHTML = booksHTML;
-  console.log(booksHTML)
-  // booksWrapper.innerHTML =
-    
+}
+
+function ratingsHTML(rating){
+  let ratingHTML = ``;
+
+  for (let i = 0; i < Math.floor(rating); i++){
+    ratingHTML += `<i class="fas fa-star"></i>\n`
+  }
+
+  if (!Number.isInteger(rating)) {
+    ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`
+  }
+  return ratingHTML
+}
+
+function filterBooks(event){
+    renderBooks(event.target.value)
 }
 
 setTimeout(() => {
