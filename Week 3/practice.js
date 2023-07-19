788449c59750cf2199f69fd4ea2684d65b8d07f8
Kevin Mendez <mediaman502@aol.com>
2023-07-18 21:29:31 -0400

    making progess

--- Week 3/practice.js
+++
@@ -1,8 +0,0 @@
-function main () {
-    let store = document.querySelector(`h1`);
-    store.innerHTML = store.innerHTML === 'Hello World' ? 'Kevin was here' : 'Hello World';
-}
-
-function toggleDark (){
-    document.querySelector(`body`).classList.toggle(`theme`)
-}
