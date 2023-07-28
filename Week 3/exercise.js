0242fd34c8d71dbf64f7c68091c1d8d93ffb3373
Kevin Mendez <mediaman502@aol.com>
2023-07-28 13:34:14 -0400

    relearning

--- Week 3/exercise.js
+++ Week 3/exercise.js
@@ -1,1 +1,19 @@
-console.log(`Hello World`)
+let str = `Frontend Simplifed`
+
+for (let i = 1; i <= 20; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+        console.log(`${i} <--- Frontend Simplifed`)
+    }
+
+    else if (i % 5 === 0) {
+        console.log(`${i} <--- Simplifed`)
+    }
+
+    else if (i % 3 === 0) {
+        console.log (`${i} <--- Frontend`)
+    }
+
+    else {
+        console.log(`${i} <--- ${i}`)
+    }
+}
