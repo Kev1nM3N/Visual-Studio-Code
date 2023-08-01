013070c7318d256f302904af8a1674e5042ee60b
Kevin Mendez <mediaman502@aol.com>
2023-08-01 15:39:32 -0400

    good review

--- Week 3/exercise.js
+++ Week 3/exercise.js
@@ -1,5 +1,18 @@
-function changeBackgroundColor() {
-    let changeColor = document.querySelector(`body`);
-    changeColor.classList.toggle(`dark-theme`);
-}
+// async function main (){
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+//     const data = await response.json()
+//     const store = data.filter(element => !element.completed).slice(0, 6);
+//     console.log(store)
+// }
 
+// main()
+
+fetch (`https://jsonplaceholder.typicode.com/todos`)
+
+.then ((response) => {
+    return response.json()
+})
+.then((data) => {
+    const store = data.filter(element => !element.completed).slice(0, 6)
+    console.log(store);
+})
