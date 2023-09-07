520866dcedff45cfa7c71cfa58f904cbbb44a390
Kevin Mendez <mediaman502@aol.com>
2023-09-07 13:41:29 -0400

    still working

--- Week 4/Final Project/final.js
+++ Week 4/Final Project/final.js
@@ -2,6 +2,9 @@ const movieElement = document.querySelector(`.movies`)
 const searchInput = document.getElementById(`showcase__searchbar`)
 const changeHeading = document.querySelector(`.movies__header--title`)
 const filterBox = document.getElementById(`filter`)
+const mainMoviesElement = document.getElementById(`main__movies`)
+const rowMovies = document.querySelector(`row__movies`)
+const spinningWheel = document.querySelector(`.fa-spinner`)
 
 // https://www.omdbapi.com/?apikey=ae3e12f8&s=
 
@@ -53,6 +56,9 @@ function movieSearch (event){
 }
 
 async function movieSearchSubmit(newFilter) {
+    let searchMovies
+    let searchMoviesResult
+
     const searchText = searchInput.value;
     if (!searchText){
         return alert(`Please Enter a Valid Movie`)
@@ -62,29 +68,47 @@ async function movieSearchSubmit(newFilter) {
 
     const id = localStorage.getItem(`apple`)
 
-    const searchMovies = await fetch (`https://www.omdbapi.com/?apikey=ae3e12f8&s=${id}`);
-    const searchMoviesResult = await searchMovies.json();
 
-    filterBox.selectedIndex = ""
-
-    changeHeading.innerHTML = `Results For: ${id}`
-    const filteredMovies = searchMoviesResult.Search.filter(element => /^[0-9]+$/.test(element.Year));
-
-    if (newFilter === `New_to_Old`){
-        filteredMovies.sort((a, b) => (b.Year) - (a.Year));
-        filterBox.selectedIndex = 1;
-    }
-    else if (newFilter === `Old_to_New`){
-        filteredMovies.sort((a, b) => (a.Year) - (b.Year));
-        filterBox.selectedIndex = 2;
+    if (movieElement){
+        movieElement.setAttribute(`style`, `display: none`)
+        spinningWheel.setAttribute(`style`, `display: block !important`)
     }
 
-    filteredMovies.length > 0
-        ? movieElement.innerHTML = filteredMovies.map((element) => moviesHTML(element)).join(``)
-        : movieElement.innerHTML = `<p>No results found.</p>`;
+    setTimeout(() => {
+        spinningWheel.removeAttribute(`style`, `display: block !important`)
+        movieElement.removeAttribute(`style`, `display: block`)
+        changeHeading.innerHTML = `Results For: ${id}`
+    }, 3000);
+    
+    
+    searchMovies = await fetch (`https://www.omdbapi.com/?apikey=ae3e12f8&s=${id}`);
+    searchMoviesResult = await searchMovies.json();
 
-    console.log(searchMoviesResult);
+    
+    if (!searchMoviesResult || !searchMoviesResult.Search) {
+        movieElement.innerHTML = `<p>No results found.</p>`;
+    } else {
+        filterBox.selectedIndex = "";
+
+        changeHeading.innerHTML = `Fetching...`
+        const filteredMovies = searchMoviesResult.Search.filter(element => /^[0-9]+$/.test(element.Year));
+
+        if (newFilter === `New_to_Old`){
+            filteredMovies.sort((a, b) => (b.Year) - (a.Year));
+            filterBox.selectedIndex = 1;
+        }
+        else if (newFilter === `Old_to_New`){
+            filteredMovies.sort((a, b) => (a.Year) - (b.Year));
+            filterBox.selectedIndex = 2;
+        }
+        
+        filteredMovies.length > 0
+            ? movieElement.innerHTML = filteredMovies.map((element) => moviesHTML(element)).join(``)
+            : movieElement.innerHTML = `<p>No results found2.</p>`;
+        console.log(filteredMovies);
+    }
 }
+
 
 function filterMovies (event){
     const searchText = searchInput.value;
