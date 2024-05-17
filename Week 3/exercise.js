e1a719ccf431e55a45ad81ab8d32d824f1dd7d34
Kevin Mendez <mediaman502@aol.com>
2024-05-17 00:36:15 -0400

    added images

--- Week 3/exercise.js
+++ Week 3/exercise.js
@@ -1,3 +1,37 @@
 //https://tarotapi.dev/api/v1/cards
-//https://jsonplaceholder.typicode.com/posts
 
+async function renderCards (){
+    let response = await fetch(`https://tarotapi.dev/api/v1/cards`);
+    let data = await response.json();
+    let allCards = data.cards;
+    let majorCards = allCards.filter((element) => element.type === "major");
+
+    let storeIn = majorCards.splice(-2, 1);
+    storeIn[0].value = "0";
+    majorCards.unshift(storeIn[0]);
+    majorCards.find((element) => (element.name === "Fortitude" ? (element.name = "Strength") : null));
+    majorCards.find((element) => (element.name === "The Last Judgment" ? (element.name = "Judgement") : null));
+    let newMajorCards = majorCards;
+
+    let mergedCards = [...newMajorCards, ...allCards.filter((card) => card.type !== "major")];
+    let majorMergedCards = mergedCards.filter((element) => element.type === "major");
+    let minorMergedCards = mergedCards.filter((element) => element.type === "minor");
+
+    minorMergedCards.find((element) => {
+       if (["page", "knight", "queen", "king"].includes(element.value)){
+            element.category = "face";
+       }
+    });
+
+    minorMergedCards.forEach((element) => {
+        if (element.category === "face"){
+            element.image = `./assets/faceCards/${element.name}.jpg`;
+        }
+    })
+    
+
+    console.log(minorMergedCards);
+
+}
+
+renderCards();
--- Week 4/Final/tarot.js
+++ Week 4/Final/tarot.js
@@ -1,1 +1,65 @@
-console.log(`working`);
+//https://tarotapi.dev/api/v1/cards
+
+let cardList = document.querySelector(`.card__list`);
+
+async function renderCards (){
+    let response = await fetch(`https://tarotapi.dev/api/v1/cards`);
+    let data = await response.json();
+    let allCards = data.cards;
+    let onlyMajorCards = allCards.filter((element) => element.type === "major");
+
+    //editing the cards to my liking
+
+    let storeIn = onlyMajorCards.splice(-2, 1);
+    storeIn[0].value = "0";
+    onlyMajorCards.unshift(storeIn[0]);
+    onlyMajorCards.find((element) => (element.name === "Fortitude" ? (element.name = "Strength") : null));
+    onlyMajorCards.find((element) => (element.name === "The Last Judgment" ? (element.name = "Judgement") : null));
+    let newMajorCards = onlyMajorCards;
+
+    let mergedCards = [...newMajorCards, ...allCards.filter((card) => card.type !== "major")];
+    let majorMergedCards = mergedCards.filter((element) => element.type === "major");
+    let minorMergedCards = mergedCards.filter((element) => element.type === "minor");
+
+    minorMergedCards.find((element) => {
+        if (["page", "knight", "queen", "king"].includes(element.value)){
+             element.category = "face";
+        }
+    });
+
+    let faceMergedCards = minorMergedCards.filter((element) => element.category);
+
+    //now adding images to every card
+
+    mergedCards.forEach((card) => {
+        if (card.type === "major") {
+            card.image = `./assets/tarotImages/majorArcana/${card.name}.jpg`;
+        }
+        if (card.type === "minor"){
+            if (!card.category){
+                card.image = `./assets/tarotImages/minorArcana/${card.name}.jpg`;
+            }
+            else if (card.category){
+                card.image = `./assets/tarotImages/faceImages/${card.name}.jpg`
+            }
+        }
+    })
+    console.log(mergedCards);
+
+    //Editing the DOM
+    cardList.innerHTML = mergedCards.map((element) => renderCardsHTML(element)).join(``);
+}
+
+renderCards();
+
+function renderCardsHTML (element) {
+    return `<div class="card">
+                <figure class="card__img--wrapper">
+                    <img src="${element.image}" alt="" class="tarot__img">
+                </figure>
+
+                <p class="tarot__name">${element.name}</p>
+                <p class="tarot__category">${element.type.toUpperCase()}</p>
+                <p class="tarot__number">${element.value}</p>
+            </div>`
+}
